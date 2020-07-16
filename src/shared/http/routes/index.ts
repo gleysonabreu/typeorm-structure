68c4d9936e814a8c643e2b0b9704dfac165a86fd
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-07-16 11:46:57 -0300

    delete folder http

--- src/shared/http/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import userRoutes from '@modules/user/routes/users.routes';
-
-const routes = Router();
-
-routes.use('/user', userRoutes);
-
-export default routes;
